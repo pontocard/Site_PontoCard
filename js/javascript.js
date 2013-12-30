64fc18efe475aa1649f31fbbec6ba27e248e75e1
pontocard <suporte3@pontocardce.com.br>
2013-12-30 14:41:00 -0300

    Requisições Ajax
    Inclusão de código Ajax em arquivo javascript.js para partilha de
    conteúdo entre páginas; correção de erros gerais e parâmetros vazios;
    retirada de quebras de linha desnecessárias em arquivo style.css; etc.

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,36 @@
 function promocao(){
-	window.open('http://www.pontocardce.com.br/promocao-mes.html', 'okno', 'width=600,height=510,status=no,toolbar=no, menubar=no,scrollbars=auto,resizable=yes');
+	window.open('promocao-mes.html','okno','width=600,height=510,status=no,toolbar=no,menubar=no,scrollbars=auto,resizable=yes');
+}
+function pagina(){
+	$.ajax({ // Copia #top-bar e #header
+		url:'acessorios.html',
+		success:function(copia){
+			copia=$(copia).find('div#top-bar,div#header');
+			$('#itens-copiados-topo').html(copia);
+		}
+	});
+	$.ajax({ // copia #footer-esquerda e #footer-meio
+		url:'acessorios.html',
+		success:function(copia2){
+			copia2=$(copia2).find('div#footer-esquerda,div#footer-meio');
+			$('#itens-copiados-footer').html(copia2);
+		},
+		error:function(){
+			alert('Pedimos desculpas, mas os componentes da página não foram carregados totalmente. Por favor, atualize a página.');
+		}
+	});
+	$.ajax({ // copia #copyrights
+		url:'acessorios.html',
+		success:function(copia3){
+			copia3=$(copia3).find('div#copyrights');
+			$('#direitos-autorais').html(copia3);
+		}
+	});
+	$.ajax({ // copia marquee
+		url:'acessorios.html',
+		success:function(copia4){
+			copia4=$(copia4).find('marquee');
+			$('#promo-button').html(copia4);
+		}
+	});
 }
